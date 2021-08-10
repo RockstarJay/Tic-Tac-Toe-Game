baa5ed77ee42f59552e91cd7ed34e2ddaa79a124
unknown <RockstarJay>
2021-08-10 08:33:43 +0530

    Added: created a board for tic tac toe game

--- src/com/jaybhagat/workshop/tictactoegame/TicTacToeGame.java
+++ src/com/jaybhagat/workshop/tictactoegame/TicTacToeGame.java
@@ -1,8 +1,22 @@
 package com.jaybhagat.workshop.tictactoegame;
 
+/*
+ * class to print the tic tac toe game board
+ * @author com.jaybhagat
+ */
+
 public class TicTacToeGame {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game");
+		createBoard();
+	}
+	
+	private static char[] createBoard() {
+		char[] board = new char[10];
+		for(int index = 0; index < board.length; index++) {
+			board[index] = ' ';
+		}
+		return board;
 	}
 }
