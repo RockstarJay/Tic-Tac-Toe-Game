c994d67b206b90a6fdb298bc67c3a79b0c2f5d9c
unknown <RockstarJay>
2021-08-19 15:04:12 +0530

    Added: method to see the board

--- src/com/jaybhagat/workshop/tictactoegame/TicTacToeGame.java
+++ src/com/jaybhagat/workshop/tictactoegame/TicTacToeGame.java
@@ -8,15 +8,18 @@ import java.util.Scanner;
  */
 
 public class TicTacToeGame {
+	public static char[] board;
 	public static char playerOption,computerOption;
 	public static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game");
 		//function call
-		char[] board = createBoard();
+		 createBoard();
 		// method call to choose the option
 		chooseXorO();
+		// method call to see the board
+		currentBoard();
 	}
 	
 	/*
@@ -39,9 +42,34 @@ public class TicTacToeGame {
 		 System.out.print("Choose an Option x or o : ");
 		 playerOption = scanner.next().charAt(0);
 		 if (playerOption == 'x')
-			 computerOption='o';
+			 computerOption = 'o';
 		 else
-			 computerOption ='x';
+			 computerOption = 'x';
 		 System.out.println("You Selected : " +playerOption);
 	 }
+	
+	/**
+	 * displays board
+	 * number are assigned with marks and numbers
+	 */
+	private static void showBoard() {
+		System.out.println("\n  " + board[1] + " | " + board[2] + " | " + board[3] + " ");
+		System.out.println(" ----------- ");
+		System.out.println("  " + board[4] + " | " + board[5] + " | " + board[6] + " ");
+		System.out.println(" ----------- ");
+		System.out.println("  " + board[7] + " | " + board[8] + " | " + board[9] + " \n");
+	}
+	
+	/**
+	 * assigning elements with numbers if it is not played by the user
+	 */
+	private static void currentBoard() {
+		board = new char[10];
+		for (int i = 1; i < 10; i++) {
+			if (board[i] != 'X' && board[i] != 'O') {
+				board[i] = (char) ( i + '0');
+			}
+		}
+	    showBoard();
+	}
 }
